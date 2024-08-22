6b499dd6d38d1546051d1c5d6b6afd6537e35447
kemily1616 <126689938+kemily1616@users.noreply.github.com>
2024-08-22 10:42:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,49 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta1",
+        enunciado: "Como um torcedor pode afetar uma estação de trem inteira por causa de uma aposta errada?",
 
-        alternativas: ["Alternativa 1",
-            "Alternativa 2"
+        alternativas: ["Fazer com que o ambiente seja mais perigoso e violento, podendo resultar em feridos e até óbitos.",
+            
+            "Pode quebrar uma coisa ou outra da estação, mas logo pode ser contido e preso pela polícia.",
         ]
     },
 
+    {
+        enunciado: "Que medidas de segurança devem ser tomadas contra brigas de torcedores em relação a população que utiliza  os transportes públicos ?",
+        alternativas: [" Outra medida de para combater a violência é que um agente de segurança acompanhem os torcedores nos transportes públicos/privados para que a deslocação seja feita de forma pacífica" ,
+           
+            " No ingresso deve ser cobrado uma taxa adicional referente a um frete de ônibus, assim após o final da partida os torcedores serão direcionados aos seus respectivos transportes, evitando conflitos" , 
+        ]
+    }, 
+    {
+        enunciado: "Como as apostas esportivas podem influenciar na taxa de violência urbana em transportes públicos",
+        alternativas: ["Influência de Grupos de Aposta",
+            
+            "Aumento da Tensão em Áreas de Jogo",
+        ]
+    }, 
     {
         enunciado: "Pergunta2",
         alternativas: ["Alternativa 1",
             "Alternativa 2"
         ]
-    }
+    }, 
+    {
+        enunciado: "Pergunta2",
+        alternativas: ["Alternativa 1",
+            "Alternativa 2"
+        ]
+    }, 
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+}
+
+mostraPergunta();
